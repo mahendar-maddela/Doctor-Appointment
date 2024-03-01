95536cc54acd8a4cd7affcea2bc36209a7eaa164
Mahendar maddela <mahendar1241@gmail.com>
2024-03-01 16:07:31 +0530

    doctor appointment

--- Doctor_Appointments/src/main/java/com/Appointments/Doctor_Appointments/controller/AppointmentController.java
+++ Doctor_Appointments/src/main/java/com/Appointments/Doctor_Appointments/controller/AppointmentController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins ="http://localhost:3000")
+@CrossOrigin(origins ="http://localhost:30003")
 @RestController
 @RequestMapping("/api/v1")
 public class AppointmentController {
